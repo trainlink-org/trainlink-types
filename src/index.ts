d3f2ae94c3b79e06cfc249f3aa6deee4600e4cf1
matt-hu <matt.hu1@outlook.com>
2023-05-27 11:32:08 +0100

    Feat: Add initial code

---
+++ src/index.ts
@@ -0,0 +1,194 @@
+
+/**
+ * Describes the direction of a Loco
+ */
+export enum Direction {
+    Forward = 'Forward',
+    Stopped = 'Stopped',
+    Reverse = 'Reverse'
+}
+
+export enum AutomationErrorType {
+    syntaxError = 'Syntax Error',
+    unknownError = 'Unknown Error',
+}
+
+export class AutomationError {
+    
+    readonly type: AutomationErrorType;
+    readonly message: string;
+    readonly location?: string;
+
+
+    constructor(type: AutomationErrorType, msg: string, location?: string) {
+        this.type = type;
+        this.message = msg;
+        this.location = location;
+    }
+}
+
+export type locoIdentifier = string | number;
+export type jsonString = string;
+
+export interface ServerToClientEvents {
+    'metadata/handshake': (appName: string, version: string ) => void;
+    'metadata/initialState/trackPower': (state: boolean) => void;
+    'metadata/initialState/locos': (locos: jsonString[]) => void;
+    'metadata/initialState/turnouts': (packet: TurnoutPacket) => void;
+    'automation/fetchAllResponse': (automations: AutomationScriptClient[]) => void;
+    'automation/fetchRunningResponse': (automations: RunningAutomationClient[]) => void;
+    'automation/processingError': (error: AutomationError) => void;
+    'throttle/speedUpdate': (identifier: locoIdentifier, speed: number, socketId: string, throttleNum: number) => void;
+    'throttle/directionUpdate': (identifier: locoIdentifier, direction: Direction, id?: string) => void;
+    'throttle/functionUpdate': (identifier: locoIdentifier, functionNum: number, state: boolean) => void;
+    'throttle/trackPowerUpdate': (state: boolean, socketId: string) => void;
+    'routes/turnoutUpdate': (identifier: number, state: TurnoutState) => void;
+    'routes/routeUpdate': (route: RouteObject) => void;
+    'routes/setRouteComponents': (destinations: number[], turnouts: number[], links: number[]) => void;
+    'routes/unsetRouteComponents': (destinations: number[], turnouts: number[], links: number[]) => void;
+    'config/newLocoAdded': (loco: jsonString) => void;
+    'config/locoEdited': (oldAddress: number, newAddress: number, name: string) => void;
+    'config/locoDeleted': (address: number) => void;
+}
+
+export interface ClientToServerEvents {
+    'metadata/handshake': (appName: string, version: string ) => void;
+    'automation/fileUpload': (name: string, file: string) => void;
+    'automation/fetchAll': () => void;
+    'automation/fetchRunning': () => void;
+    'automation/setDescription': (id: number, description: string) => void;
+    'automation/pauseAutomation': (pid: PID) => void;
+    'automation/resumeAutomation': (pid: PID) => void;
+    'automation/stopAutomation': (pid: PID) => void;
+    'automation/executeAutomation': (id: number, locoId?: locoIdentifier) => void;
+    'automation/deleteAutomation': (id: number) => void;
+    'throttle/setSpeed': (identifier: locoIdentifier, speed: number, throttleID: number) => void;
+    'throttle/switchDirection': (identifier: locoIdentifier) => void;
+    'throttle/setDirection': (identifier: locoIdentifier, direction: Direction) => void;
+    'throttle/setTrackPower': (state: boolean) => void;
+    'throttle/setFunction': (identifier: locoIdentifier, functionNum: number, state: boolean) => void;
+    'routes/setTurnout': (identifier: number, state: TurnoutState) => void;
+    'routes/setRoute': (start: number, end: number) => void;
+    'config/addLoco': (name: string, address: number) => void;
+    'config/editLoco': (oldAddress: number, newName: string, newAddress: number) => void;
+    'config/deleteLoco': (address: number) => void;
+    'config/routes/addObject': (turnout: MapPoint) => void;
+    'config/routes/changeObjectCoordinate': (id: number, coordinate: Coordinate) => void;
+}
+
+/**
+ * Base type for a hardware adapter.
+ */
+export interface HardwareAdapter {
+    locoSetSpeed(address: number, speed:number, direction:Direction): Promise<void>,
+    locoEstop(address: number): Promise<void>,
+    turnoutSet(id: number, state: TurnoutState): Promise<void>,
+}
+
+export enum AutomationType {
+    Sequence = 'Sequence',
+    Route = 'Route',
+    Automation = 'Automation',
+    EventHandler = 'Event Handler'
+}
+
+export enum EventHandlerType {
+    Turnout = 'Turnout',
+    None = 'None'
+}
+
+export interface AutomationScriptClient {
+    name: string,
+    id: number,
+    description: string,
+    type: AutomationType,
+    source: string,
+}
+
+export interface RunningAutomationClient {
+    name: string,
+    description: string,
+    type: AutomationType,
+    status: AutomationStatus,
+    locoAddress?: number,
+    pid: PID
+}
+
+export type PID = string;
+
+export enum AutomationStatus {
+    Running = 'Running',
+    Paused = 'Paused'
+}
+
+export enum TurnoutState {
+    closed = 'Closed',
+    thrown = 'Thrown'
+}
+
+export interface TurnoutPacket {
+    turnouts: Turnout[],
+    links: TurnoutLink[],
+    destinations: Destination[],
+}
+
+export interface MapPoint {
+    id: number,
+    coordinate: Coordinate,
+}
+
+export interface Turnout extends MapPoint {
+    name: string,
+    state: TurnoutState,
+    primaryDirection: number,
+    secondaryDirection: number,
+    connections: number[],
+}
+
+export interface TurnoutLink {
+    id: number,
+    length: number,
+    start: number, //Turnout.id || Destination.id
+    end: number, //Turnout.id
+    points: Coordinate[],
+    startActive: boolean,
+    endActive: boolean,
+}
+
+export interface Destination extends MapPoint {
+    /** A negative id for the destination (Not exposed to user) */
+    id: number, // Should always be -ve
+    name: string,
+    description: string,
+    connections: number[],
+}
+
+export interface Coordinate{
+    x: number,
+    y: number,
+}
+
+export interface RouteObject {
+    start: Destination,
+    turnouts: CurrentTurnoutState[],
+    links: TurnoutLink[],
+    end: Destination
+}
+
+export interface CurrentTurnoutState {
+    id: number,
+    state: TurnoutState,
+}
+
+/**
+ * Checks if an object has the correct properties to be a {@link Turnout}
+ * @param object Object to check
+ * @returns True or false
+ */
+export function isTurnout(object: unknown): object is Turnout {
+    if (typeof object === 'object' && object) {
+        return 'primaryDirection' in object && 'secondaryDirection' in object;
+    } else {
+        return false;
+    }
+}
